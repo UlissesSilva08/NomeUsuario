33dc92fb3a310ecbb265530ef36b2515fd726830
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-08-29 14:34:20 -0300

    Adicionando as funções string para receber a entrada do usuário e exibindo via comando WriteLine na forma completa e catálogada

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NomeUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, Usuário");
+            Console.WriteLine("----------------------");
+            Console.Write("Por favor, digite seu nome: ");
+
+            string nome = Console.ReadLine();
+
+            Console.Write("Olá, ");
+            Console.WriteLine(nome);
+            Console.WriteLine();
+
+            Console.Write("Agora digite seu sobrenome: ");
+            string sobrenome = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Seu nome completo é; " + nome + " " + sobrenome);
+            Console.Write("Seu nome de catálago é: ");
+            Console.Write($"{sobrenome.ToUpper()}"  + ", " + $"{nome.ToString()}");
+        }
+    }
+}
